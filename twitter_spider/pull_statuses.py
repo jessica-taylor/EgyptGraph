d677de6ed24e9d20551553f1f3b0b0cb957ab540
Asheem <Asheem@DN0a160b79.SUNet>
2011-05-14 15:50:59 -0700

    Added twitter authentication keys, status pull script

---
+++ twitter_spider/pull_statuses.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+# Python script to pull twitter status posts from a given user.
+# Just trying out the python-twitter library.
+# Depends on said library, so get it here:
+# http://code.google.com/p/python-twitter/
+
+print "Twitter Spider"
+
+tw_no = 0  #tweet iterator
+
+import twitter
+api = twitter.Api()
+
+atl_status = api.GetUserTimeline("as_te_li")
+
+for s in atl_status:
+	tw_no = tw_no+1
+	print "Tweet #",tw_no,":",s.text,""
+
